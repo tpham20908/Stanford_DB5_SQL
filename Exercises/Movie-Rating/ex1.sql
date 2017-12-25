95ffc3ef6481f8282b16fd9c7cdfe38d42fc60e9
Tung Pham <tpham20908@gmail.com>
2017-12-24 22:12:03 -0500

    finish question4 exercise1 Movie-Rating

--- Exercises/Movie-Rating/ex1.sql
+++ Exercises/Movie-Rating/ex1.sql
@@ -17,6 +17,43 @@
 -- Q4: Some reviewers didn't provide a date with their rating.
 -- Find the names of all reviewers who have ratings with a
 -- NULL value for the date.
-select name from reviewer
-where rID in (select rID from Rating
-              where ratingDate is null);
+-- select name from reviewer
+-- where rID in (select rID from Rating
+--               where ratingDate is null);
+
+-- -- Q5: Write a query to return the ratings data in a
+-- -- more readable format: reviewer name, movie title, stars,
+-- -- and ratingDate. Also, sort the data, first by reviewer
+-- -- name, then by movie title, and lastly by number of stars.
+-- select name, title, stars, ratingDate
+-- from Movie m, Reviewer rv, Rating rt
+-- where m.mID = rt.mID and rv.rID = rt.rID
+-- order by rv.name, m.title, rt.stars;
+
+-- -- Q6: For all cases where the same reviewer rated the
+-- -- same movie twice and gave it a higher rating the second
+-- -- time, return the reviewer's name and the title of the movie.
+-- select name, title
+-- from Movie, Reviewer,
+--   (select rID, mID
+--   from Rating R1
+--   where mID in
+--     (select mID from Rating R2
+--     where R1.mID = R2.mID
+--     and R1.rID = R2.rID
+--     and R1.stars < R2.stars
+--     and R1.ratingDate < R2.ratingDate)) as R
+-- where R.rID = Reviewer.rID and R.mID = Movie.mID;
+
+-- Q7: For each movie that has at least one rating,
+-- find the highest number of stars that movie received.
+-- Return the movie title and number of stars. Sort by
+-- movie title.
+select distinct title, stars
+from Movie, Rating R1
+where not exists
+    (select * from Rating R2
+    where R1.mID = R2.mID
+    and R2.stars > R1.stars)
+  and Movie.mID = R1.mID
+  order by title;

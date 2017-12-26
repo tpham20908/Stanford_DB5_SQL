a7beca960cbd50f3d4421298d8f74a5957f6bb39
Tung Pham <tpham20908@gmail.com>
2017-12-25 22:16:42 -0500

    Q3 ex2 Movie-Rating

--- Exercises/Movie-Rating/ex2.sql
+++ Exercises/Movie-Rating/ex2.sql
@@ -4,9 +4,18 @@
 -- where Reviewer.rID = Rating.rID and Rating.mID = Movie.mID
 --       and title = 'Gone with the Wind';
 
--- Q2: For any rating where the reviewer is the same as the director of
--- the movie, return the reviewer name, movie title, and number of stars.
-select distinct name, title, stars
-from Movie, Reviewer, Rating
-where name = director and Movie.mID = Rating.mID
-      and Reviewer.rID = Rating.rID;
+-- -- Q2: For any rating where the reviewer is the same as the director of
+-- -- the movie, return the reviewer name, movie title, and number of stars.
+-- select distinct name, title, stars
+-- from Movie, Reviewer, Rating
+-- where name = director and Movie.mID = Rating.mID
+--       and Reviewer.rID = Rating.rID;
+
+-- Q3: Return all reviewer names and movie names together in a single list,
+-- alphabetized. (Sorting by the first name of the reviewer and first word in
+-- the title is fine; no need for special processing on last names or removing "The".)
+select name
+from Reviewer
+union
+select title
+from Movie;

d1e5da6f592f75c8aa5182b7eaceef6750d93197
Tung Pham <tpham20908@gmail.com>
2017-12-25 22:00:08 -0500

    Q2 ex2 Movie-Rating

--- Exercises/Movie-Rating/ex2.sql
+++ Exercises/Movie-Rating/ex2.sql
@@ -1,5 +1,12 @@
--- Q1: Find the names of all reviewers who rated Gone with the Wind.
-select distinct name
-from Reviewer, Movie, Rating
-where Reviewer.rID = Rating.rID and Rating.mID = Movie.mID
-      and title = 'Gone with the Wind';
+-- -- Q1: Find the names of all reviewers who rated Gone with the Wind.
+-- select distinct name
+-- from Reviewer, Movie, Rating
+-- where Reviewer.rID = Rating.rID and Rating.mID = Movie.mID
+--       and title = 'Gone with the Wind';
+
+-- Q2: For any rating where the reviewer is the same as the director of
+-- the movie, return the reviewer name, movie title, and number of stars.
+select distinct name, title, stars
+from Movie, Reviewer, Rating
+where name = director and Movie.mID = Rating.mID
+      and Reviewer.rID = Rating.rID;

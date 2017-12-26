0223652f241375ee87b329b58fa791a5336ed481
Tung Pham <tpham20908@gmail.com>
2017-12-26 10:10:32 -0500

    Q5 ex2 Movie-Rating

--- Exercises/Movie-Rating/ex2.sql
+++ Exercises/Movie-Rating/ex2.sql
@@ -20,8 +20,23 @@
 -- select title
 -- from Movie;
 
--- Q4: Find the titles of all movies not reviewed by Chris Jackson.
-select title from Movie
-where mID not in (select mID from Reviewer, Rating
-                  where Reviewer.rID = Rating.rID
-                        and name = 'Chris Jackson');
+-- -- Q4: Find the titles of all movies not reviewed by Chris Jackson.
+-- select title from Movie
+-- where mID not in (select mID from Reviewer, Rating
+--                   where Reviewer.rID = Rating.rID
+--                         and name = 'Chris Jackson');
+
+-- Q5: For all pairs of reviewers such that both reviewers gave a rating to the same
+-- movie, return the names of both reviewers. Eliminate duplicates, don't pair reviewers
+-- with themselves, and include each pair only once. For each pair, return the names in
+-- the pair in alphabetical order.
+select distinct L1.name, L2.name
+from (select distinct name, title
+      from Movie, Reviewer, Rating
+      where Movie.mID = Rating.mID
+      and Reviewer.rID = Rating.rID) L1,
+      (select distinct name, title
+      from Movie, Reviewer, Rating
+      where Movie.mID = Rating.mID
+      and Reviewer.rID = Rating.rID) L2
+where L1.title = L2.title and L1.name < L2.name;

6716fd6aeebdf1bad4ac0d404a92cb892aa322c1
Tung Pham <tpham20908@gmail.com>
2017-12-28 17:54:37 -0500

    Q2 ex4 Social-Network Extras

--- Exercises/Social-Network/ex4.sql
+++ Exercises/Social-Network/ex4.sql
@@ -1,7 +1,20 @@
--- Q1: For every situation where student A likes student B, but student B likes a different student C, return the names and grades of A, B, and C.
-select H1.name, H1.grade, H2.name, H2.grade, H3.name, H3.grade
-from Highschooler H1 join Likes L1 on H1.ID = L1.ID1
-     join Highschooler H2 on L1.ID2 = H2.ID
-     join Likes L2 on H2.ID = L2.ID1
-     join Highschooler H3 on L2.ID2 = H3.ID
-where H1.ID <> H3.ID;
+-- -- Q1: For every situation where student A likes student B, but student B likes a different
+-- -- student C, return the names and grades of A, B, and C.
+-- select H1.name, H1.grade, H2.name, H2.grade, H3.name, H3.grade
+-- from Highschooler H1 join Likes L1 on H1.ID = L1.ID1
+--      join Highschooler H2 on L1.ID2 = H2.ID
+--      join Likes L2 on H2.ID = L2.ID1
+--      join Highschooler H3 on L2.ID2 = H3.ID
+-- where H1.ID <> H3.ID;
+
+-- Q2: Find those students for whom all of their friends are in different grades from themselves.
+-- Return the students' names and grades.
+select H1.name, H1.grade
+from Highschooler H1 join Friend on H1.ID = ID1
+      join Highschooler H2 on ID2 = H2.ID
+where H1.grade <> H2.grade
+except
+select H1.name, H1.grade
+from Highschooler H1 join Friend on H1.ID = ID1
+      join Highschooler H2 on ID2 = H2.ID
+where H1.grade = H2.grade;

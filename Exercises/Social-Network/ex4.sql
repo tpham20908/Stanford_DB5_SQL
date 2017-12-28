22834ec360fec844026c2475d476f81ed43e3bfa
Tung Pham <tpham20908@gmail.com>
2017-12-28 18:02:49 -0500

    Q3 ex4 Social-Network Extras

--- Exercises/Social-Network/ex4.sql
+++ Exercises/Social-Network/ex4.sql
@@ -7,14 +7,20 @@
 --      join Highschooler H3 on L2.ID2 = H3.ID
 -- where H1.ID <> H3.ID;
 
--- Q2: Find those students for whom all of their friends are in different grades from themselves.
--- Return the students' names and grades.
-select H1.name, H1.grade
-from Highschooler H1 join Friend on H1.ID = ID1
-      join Highschooler H2 on ID2 = H2.ID
-where H1.grade <> H2.grade
-except
-select H1.name, H1.grade
-from Highschooler H1 join Friend on H1.ID = ID1
-      join Highschooler H2 on ID2 = H2.ID
-where H1.grade = H2.grade;
+-- -- Q2: Find those students for whom all of their friends are in different grades from themselves.
+-- -- Return the students' names and grades.
+-- select H1.name, H1.grade
+-- from Highschooler H1 join Friend on H1.ID = ID1
+--       join Highschooler H2 on ID2 = H2.ID
+-- where H1.grade <> H2.grade
+-- except
+-- select H1.name, H1.grade
+-- from Highschooler H1 join Friend on H1.ID = ID1
+--       join Highschooler H2 on ID2 = H2.ID
+-- where H1.grade = H2.grade;
+
+-- Q3: What is the average number of friends per student? (Your result should be just one number.)
+select distinct
+  ((select count(*) from Friend) * 1.0) /
+  (select count(distinct ID1) from Friend)
+from Friend;

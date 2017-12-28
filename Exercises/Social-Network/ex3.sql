dabddd8df25dfbe1299faf2a2ab8c46aa34d6f5c
Tung Pham <tpham20908@gmail.com>
2017-12-27 19:51:58 -0500

    Q3 ex3 Social-Network

--- Exercises/Social-Network/ex3.sql
+++ Exercises/Social-Network/ex3.sql
@@ -3,9 +3,18 @@
 -- from Highschooler H1, Friend, Highschooler H2
 -- where H1.ID = ID1 and ID2 = H2.ID and H2.name = 'Gabriel';
 
--- Q2: For every student who likes someone 2 or more grades younger than themselves,
--- return that student's name and grade, and the name and grade of the student they like.
+-- -- Q2: For every student who likes someone 2 or more grades younger than themselves,
+-- -- return that student's name and grade, and the name and grade of the student they like.
+-- select H1.name, H1.grade, H2.name, H2.grade
+-- from Likes, Highschooler H1, Highschooler H2
+-- where H1.ID = ID1 and H2.ID = ID2
+--   and (H1.grade - H2.grade) >= 2;
+
+-- Q3: For every pair of students who both like each other, return the name and grade of both
+-- students. Include each pair only once, with the two names in alphabetical order.
 select H1.name, H1.grade, H2.name, H2.grade
-from Likes, Highschooler H1, Highschooler H2
-where H1.ID = ID1 and H2.ID = ID2
-  and (H1.grade - H2.grade) >= 2;
+from Highschooler H1, Highschooler H2, Likes
+where H1.ID = ID1 and ID2 = H2.ID
+  and H2.ID in (select ID1 from Likes)
+  and H1.ID in (select ID2 from Likes)
+  and H1.name < H2.name;

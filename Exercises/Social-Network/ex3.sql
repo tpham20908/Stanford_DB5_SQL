e9c657d2d96d53d71439324e93ca6d9b3b79b02c
Tung Pham <tpham20908@gmail.com>
2017-12-27 19:34:08 -0500

    Q2 ex3 Social-Network

--- Exercises/Social-Network/ex3.sql
+++ Exercises/Social-Network/ex3.sql
@@ -1,4 +1,11 @@
--- Q1: Find the names of all students who are friends with someone named Gabriel.
-select H1.name
-from Highschooler H1, Friend, Highschooler H2
-where H1.ID = ID1 and ID2 = H2.ID and H2.name = 'Gabriel';
+-- -- Q1: Find the names of all students who are friends with someone named Gabriel.
+-- select H1.name
+-- from Highschooler H1, Friend, Highschooler H2
+-- where H1.ID = ID1 and ID2 = H2.ID and H2.name = 'Gabriel';
+
+-- Q2: For every student who likes someone 2 or more grades younger than themselves,
+-- return that student's name and grade, and the name and grade of the student they like.
+select H1.name, H1.grade, H2.name, H2.grade
+from Likes, Highschooler H1, Highschooler H2
+where H1.ID = ID1 and H2.ID = ID2
+  and (H1.grade - H2.grade) >= 2;

c12a15b7175cecc15f12177efba51a19f68f5d10
Tung Pham <tpham20908@gmail.com>
2017-12-30 15:05:37 -0500

    30% Q3 ex6 Social-Network Extras

--- Exercises/Social-Network/ex6.sql
+++ Exercises/Social-Network/ex6.sql
@@ -2,10 +2,19 @@
 -- delete from Highschooler
 -- where grade >= 12;
 
--- Q2: If two students A and B are friends, and A likes B but not vice-versa, remove the Likes tuple.
--- select * from Likes;
-delete from Likes
-where ID2 not in
-  (select ID1 from Likes L where Likes.ID1 = L.ID2)
-  and ID2 in
-  (select ID2 from Friend where ID1 = Likes.ID1);
+-- -- Q2: If two students A and B are friends, and A likes B but not vice-versa, remove the Likes tuple.
+-- -- select * from Likes;
+-- delete from Likes
+-- where ID2 not in
+--   (select ID1 from Likes L where Likes.ID1 = L.ID2)
+--   and ID2 in
+--   (select ID2 from Friend where ID1 = Likes.ID1);
+
+-- Q3: For all cases where A is friends with B, and B is friends with C, add a new friendship
+-- for the pair A and C. Do not add duplicate friendships, friendships that already exist, or friendships
+-- with oneself. (This one is a bit challenging; congratulations if you get it right.)
+
+
+select distinct A.ID1 as ID1, C.ID2 as ID2
+from Friend A, Friend B, Friend C, Friend
+where A.ID2 = B.ID1 and B.ID2 = C.ID1 and A.ID1 <> C.ID2;

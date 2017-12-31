0b37ed97b0f16776dbf60d8194a76dd34b9fbf33
Tung Pham <tpham20908@gmail.com>
2017-12-30 22:32:00 -0500

    finish ex6 Social-Network Extras

--- Exercises/Social-Network/ex6.sql
+++ Exercises/Social-Network/ex6.sql
@@ -1,14 +1,14 @@
--- -- Q1: It's time for the seniors to graduate. Remove all 12th graders from Highschooler.
--- delete from Highschooler
--- where grade >= 12;
+-- Q1: It's time for the seniors to graduate. Remove all 12th graders from Highschooler.
+delete from Highschooler
+where grade >= 12;
 
--- -- Q2: If two students A and B are friends, and A likes B but not vice-versa, remove the Likes tuple.
--- -- select * from Likes;
--- delete from Likes
--- where ID2 not in
---   (select ID1 from Likes L where Likes.ID1 = L.ID2)
---   and ID2 in
---   (select ID2 from Friend where ID1 = Likes.ID1);
+-- Q2: If two students A and B are friends, and A likes B but not vice-versa, remove the Likes tuple.
+-- select * from Likes;
+delete from Likes
+where ID2 not in
+  (select ID1 from Likes L where Likes.ID1 = L.ID2)
+  and ID2 in
+  (select ID2 from Friend where ID1 = Likes.ID1);
 
 -- Q3: For all cases where A is friends with B, and B is friends with C, add a new friendship
 -- for the pair A and C. Do not add duplicate friendships, friendships that already exist, or friendships
